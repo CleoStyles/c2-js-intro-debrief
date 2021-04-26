69c3053a4c44f841b8334ed61634914ebedd7e48
CLEO STYLES <cleoscottstyles@gmail.com>
2021-04-26 14:36:19 -0400

    I edited js

---
+++ js/app.js
@@ -0,0 +1,32 @@
+let myNumber = 22;
+
+
+if (myNumber !== 'notAnumber') {
+    console.log('This is not a number.');
+} else if (myNumber % 15 === 0 {
+    console.log('Gen');
+} else if (myNumber % 3 === 0 {
+    console.log('Buzz');
+} else if (myNumber % 5 === 0 {
+    console.log(Genbuz);
+} else if (myNumber) {
+    console.log(Genbuz);
+
+
+
+    let storeItem = 'shoes';
+
+    if (storeItem === 'shoes') {
+        console.log('Shoes are $50.00');
+    } else if (storeItem === 'jeans') {
+        console.log('Jeans are $25.00');
+  else if (storeItem === 'hat') {
+            console.log('Hats are $12.00');
+    else if (storeItem === 'socks') {
+                console.log('Socks are $2.00');
+        
+
+
+                    let min = 50;
+                    let max = 100;
+                    console.log(Math.random(* max - min) + min));
